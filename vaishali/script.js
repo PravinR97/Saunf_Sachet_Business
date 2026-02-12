81aba9fb45e7119f442b64fb43378dba8fe34753
Pravin Raut <Pravinrautt97@gmail.com>
2026-02-12 16:39:10 +0530

    Update script.js

--- vaishali/script.js
+++ vaishali/script.js
@@ -1,1 +1,10 @@
+function correct() {
+    document.getElementById("result").innerHTML =
+    "🎉 Correct! Show this screen at counter to get discount.";
+}
+
+function wrong() {
+    document.getElementById("result").innerHTML =
+    "❌ Try again!";
+}
 
